2838b84ede20f11bde853f5b89838f150151a75f
maalek <alishayegh@pm.me>
2021-07-13 00:12:36 +0430

    Removing point A, Adding the angle of attachment line to the basal plane

--- labrossePoints.m
+++ labrossePoints.m
@@ -1,6 +1,7 @@
 function labrossePoints(x0, input)
 	%% Description
-	% Returns the coordinates of points A and B in Figs 
+	% Returns the coordinates of point B and the angle of
+	% attachment line to the basal plane in Figs 
 	% 2 and 3, Labrosse et al ("Geometric modeling of
 	% functional trileaflet aortic valves: Development and
 	% clinical applications", 2006).
@@ -15,12 +16,15 @@ function labrossePoints(x0, input)
 	% et al (2006). 
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	x = labrosse(x0, input, 0);
-	disp('A : (')
-	disp(input.Rb - (input.H - x(6))*cot(x(7)/2 + pi/4 + x(4)/2)...
-	), disp(sqrt(3)* input.Rc / 2), disp(input.H) 
-	disp(')');
+	%disp('A : (')
+	%disp(input.Rb - (input.H - x(6))*cot(x(7)/2 + pi/4 + x(4)/2)...
+	%), disp(sqrt(3)* input.Rc / 2), disp(input.H) 
+	%disp(')');
 
 	disp('B : (')
 	disp(.5 * input.Rc + x(3) * cos(x(5)))
 	disp(0), disp(input.H + x(3)*sin(x(5)))
 	disp(')');
+
+	disp('alpha + thetha / 2 (the angle of attachment line to the basal plane)[degrees]:')
+	disp(180/pi*(pi/4+x(4)/2+x(7)/2));
